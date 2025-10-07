260310870d9cc5ee703d26e117213923e23b5d5f
CharlesS <72816511+sudo-CharlesS@users.noreply.github.com>
2025-10-07 13:24:53 +0200

    test stream camera

---
+++ stream_camera.py
@@ -0,0 +1,25 @@
+import cv2
+from flask import Flask, Response
+
+app = Flask(__name__)
+camera = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L2)
+
+def generate_frames():
+        while True:
+                success, frame = camera.read()
+                if not success:
+                        break
+                else:
+                        ret, buffer = cv2.imencode('.jpg', frame)
+                        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+@app.route('/')
+def index():
+        return '<img src="/video_feed" style="width:100%">'
+
+@app.route('/video_feed')
+def video_feed():
+        return Response(generate_frames(), mimetype='multipart/x-mixed-replace;boundary=frame')
+
+if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=5000)

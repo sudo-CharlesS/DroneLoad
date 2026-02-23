808577ce6e6f4281b89ce12c43abea0e3e7aba83
CharlesS <72816511+sudo-CharlesS@users.noreply.github.com>
2026-02-23 19:08:57 +0100

    Test gstream

--- stream_camera.py
+++ stream_camera.py
@@ -32,7 +32,7 @@ FPS_out = 30                    #Video frame rate
 gst_out = (
     f"appsrc ! "
     f"videoconvert ! video/x-raw,format=I420 ! "
-    f"x264enc tune=zerolatency ! "
+    f"v4l2h264enc extra-controls=\"controls,h264_profile=4, h264_level=13, video_bitrate=4000000\" ! "
     f"rtph264pay config-interval=1 pt=96 ! "
     f"udpsink host={IP_DEST} port=5000 sync=false"
 

929bd183eb6df9a0a1b49ab034dc6144cf025b1a
CharlesS <72816511+sudo-CharlesS@users.noreply.github.com>
2026-02-24 00:22:18 +0100

    Test

--- stream_camera.py
+++ stream_camera.py
@@ -2,8 +2,10 @@ import cv2
 import time
 import sys
 
+PORT = 5000
+WIDTH, HEIGHT = 1920, 1080    #Video resolution
+FPS = 30
 
-# 1. Pipeline d'ENTRÉE (Capture 1080p -> Sortie 720p via ISP)
 # Si vous utilisez une caméra USB, remplacez libcamerasrc par v4l2src
 gst_in = (
     "v4l2src device=/dev/video0 ! "
@@ -11,24 +13,6 @@ gst_in = (
     "jpegdec ! "
     "videoconvert ! video/x-raw,format=BGR ! appsink"
 )
-#"video/x-raw,width=1280,height=720,framerate=30/1 ! "
-
-# 2. Pipeline de SORTIE (Streaming réseau UDP)
-# Remplacez l'IP par celle de votre PC récepteur
-STREAMING_ACTIVE = True
-IP_DEST = input("Type IP (or 1 for default=192.169.2.9 or 0 to deactivate streaming ) : ")#"192.168.2.9"    #Destination PC IP
-if IP_DEST == "0":
-    IP_DEST = "0.0.0.0"
-    STREAMING_ACTIVE = False
-elif IP_DEST == "1":
-    IP_DEST = "192.168.2.9"
-
-PORT = 5000
-
-WIDTH, HEIGHT = 1920, 1080    #Video resolution
-FPS = 30                    #Video frame rate
-
-# --- GStreamer Pipeline (with hardware encoding)
 gst_out = (
     f"appsrc ! "
     f"video/x-raw,format=BGR,width={WIDTH},height={HEIGHT},framerate={FPS}/1 ! "
@@ -41,19 +25,23 @@ gst_out = (
     f"udpsink host={IP_DEST} port=5000 sync=false async=false"
 )
 
-#
-
-
-# Création des objets VideoCapture et VideoWriter
 #cap = cv2.VideoCapture(gst_in, cv2.CAP_GSTREAMER)
-
 cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 cap.set(cv2.CAP_PROP_FPS, FPS)
-
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+
+STREAMING_ACTIVE = True
+IP_DEST = input("Type IP (or 1 for default=192.169.2.9 or 0 to deactivate streaming ) : ")#"192.168.2.9"    #Destination PC IP
+if IP_DEST == "0":
+    IP_DEST = "0.0.0.0"
+    STREAMING_ACTIVE = False
+elif IP_DEST == "1":
+    IP_DEST = "192.168.2.9"
+
 out = None
 if STREAMING_ACTIVE:
     out = cv2.VideoWriter(gst_out, cv2.CAP_GSTREAMER, 0, FPS, (WIDTH, HEIGHT), True)
@@ -64,8 +52,9 @@ if not cap.isOpened() or (STREAMING_ACTIVE and not out.isOpened()):
 
 print(f"Streaming vers {IP_DEST}:{PORT} en {HEIGHT}p... \n\nctrl+C to stop")
 
-start_time = time.monotonic_ns()
+prev_time = time.time()
 frame_count = 0
+fps=0
 
 try:
     while True:
@@ -75,7 +64,7 @@ try:
 
         # --- DÉBUT DU TRAITEMENT OPENCV ---
         # Exemple simple : Détection de visages ou dessin
-        cv2.putText(frame, f"Pi4 - Detection Active - Frame {frame_count}",
+        cv2.putText(frame, f"Pi4 - Detection Active - Frame {fps}",
                     (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         # Insérez votre modèle de détection ici (YOLO, Haar, etc.)
@@ -85,7 +74,13 @@ try:
         if out is not None and out.isOpened():
             out.write(frame)
 
-        frame_count += 1
+            frame_count += 1
+            current_time = time.time()
+            if current_time - prev_time >= 1.0:
+                print(f"FPS Réels : {frame_count}")
+                fps = frame_count
+                fps_count = 0
+                prev_time = current_time
 
 except KeyboardInterrupt:
     print("\nArrêt du programme...")
@@ -95,4 +90,4 @@ finally:
     cap.release()
     if out is not None and out.isOpened():
         out.release()
-    print("Pipelines fermées proprement.")
+    print("Pipelines fermées.")
